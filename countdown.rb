df21668095e41e0ea64ba138d172adb1485b7a5e
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-07 23:22:36 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  counter = 0
+  while counter > number do
+    counter -= 1
+    puts "#{number} SECOND(S)!"
+  end
+  "HAPPY NEW YEAR!"
 end
